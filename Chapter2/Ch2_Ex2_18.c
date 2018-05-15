2900189aa0451e86c7db571ed68f4b4f0f09a32e
Elle518 <teregc@hotmail.es>
2018-05-15 10:31:25 +0200

    Add exercise Ch2_Ex2_18.c

---
+++ Chapter2/Ch2_Ex2_18.c
@@ -0,0 +1,37 @@
+/*
+       Program: Ch2_Ex2_18.c
+   Descripción: This program asks the user to enter two integers, obtains the
+                numbers from the user, then prints the larger number followed by
+		          the words “is larger.” If the numbers are equal, print the
+			       message “These numbers are equal.” Use only the single-selection
+			       form of the if statement.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int number1;
+	int number2;
+	
+	puts("Please, enter two integers:");
+	scanf("%d%d", &number1, &number2);
+
+	if (number1 > number2) {
+		printf("%d is larger.\n", number1);
+	}
+
+	if (number1 < number2) {
+		printf("%d is larger.\n", number2);
+	}
+
+	if (number1 == number2) {
+		puts("These numbers are equal.");
+	}
+
+	return 0;
+}
