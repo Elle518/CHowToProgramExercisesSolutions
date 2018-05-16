3bf4465c302ef5cdd0294fc203e5f17fc1660e5c
Elle518 <teregc@hotmail.es>
2018-05-16 17:15:04 +0200

    Add exercise Ch2_Ex2_26_Multiples.c

---
+++ Chapter2/Ch2_Ex2_26_Multiples.c
@@ -0,0 +1,32 @@
+/*
+       Program: Ch2_Ex2_26_Multiples.c
+   Descripción: This program reads in two integers and determines and prints
+                whether the first is a multiple of the second. [Hint: Use the
+		          remainder operator.]
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	
+	int number1;
+	int number2;
+
+	puts("Please, enter two integers:");
+	scanf("%d%d", &number1, &number2);
+
+	if (number1 % number2 == 0) {
+		printf("%d is a multiple of %d.\n", number1, number2);
+	}
+	
+	if (number1 % number2 != 0) {
+		printf("%d is not a multiple of %d.\n", number1, number2);
+	}
+
+	return 0;
+
+}
