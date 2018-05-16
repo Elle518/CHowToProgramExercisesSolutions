1d54e9ace2a4ef8c8c18a32fcbb47df112fd0ff1
Elle518 <teregc@hotmail.es>
2018-05-16 16:04:12 +0200

    Add exercise Ch2_Ex2_25.c

---
+++ Chapter2/Ch2_Ex2_25.c
@@ -0,0 +1,34 @@
+/*
+       Program: Ch2_Ex2_25.c
+   Descripción: This program prints my initials in block letters down the page.
+                Each block letter is constructed out of the letter it represents.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+	puts("        T");
+	puts("        T");
+	puts("TTTTTTTTT");
+	puts("        T");
+	puts("        T\n");
+
+	puts("  GGGGG");
+	puts(" G     G");
+	puts("G       G");
+	puts("G  G    G");
+	puts(" GGG   G\n");
+	
+	puts("  CCCCC");
+	puts(" C     C");
+	puts("C       C");
+	puts("C       C");
+	puts(" C     C\n");
+
+	return 0;
+}
