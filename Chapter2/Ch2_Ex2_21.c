c06c1705fd8ec43a30b25e9f6e304fb447b7971d
Elle518 <teregc@hotmail.es>
2018-05-16 14:27:42 +0200

    Add exercise Ch2_Ex2_21.c

---
+++ Chapter2/Ch2_Ex2_21.c
@@ -0,0 +1,26 @@
+/*
+       Program: Ch2_Ex2_16.c
+   Descripción: This program prints shapes with asterisks.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+	puts("*********        ***          *           *");
+	puts("*       *      *     *       ***         * *");
+	puts("*       *     *       *     *****       *   *");
+	puts("*       *     *       *       *        *     *");
+	puts("*       *     *       *       *       *       *");
+	puts("*       *     *       *       *        *     *");
+	puts("*       *     *       *       *         *   *");
+	puts("*       *      *     *        *          * *");
+	puts("*********        ***          *           * ");
+
+	return 0;
+	
+}
