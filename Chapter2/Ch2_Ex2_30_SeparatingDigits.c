af5a6fb1d297e9cce0018ab52de6986133ac1eba
Elle518 <teregc@hotmail.es>
2018-05-17 13:25:02 +0200

    Add exercise Ch2_Ex2_30_SeparatingDigits.c

---
+++ Chapter2/Ch2_Ex2_30_SeparatingDigits.c
@@ -0,0 +1,29 @@
+/*
+       Program: Ch2_Ex2_30_SeparatingDigits.c
+   Descripción: This program inputs one five-digit number, separates the number
+                into its individual digits and prints the digits separated from
+		          one another by three spaces each. [Hint: Use combinations of
+			       integer division and the remainder operation.] 
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	
+	int number;
+
+	puts("Please, enter a five-digit number:");
+	scanf("%d", &number);
+
+	printf("%d   ", number/10000); // Print first digit
+	printf("%d   ", (number%10000)/1000);
+	printf("%d   ", ((number%10000)%1000)/100);
+	printf("%d   ", (((number%10000)%1000)%100)/10);
+	printf("%d\n", (((number%10000)%1000)%100)%10); // Print last digit
+
+	return 0;
+}
