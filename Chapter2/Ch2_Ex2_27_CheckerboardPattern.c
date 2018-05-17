e2b6ccf7eb08ac7757bf5db8c7091cc18435e663
Elle518 <teregc@hotmail.es>
2018-05-17 11:22:19 +0200

    Add exercise Ch2_Ex2_27_CheckerboardPattern.c

---
+++ Chapter2/Ch2_Ex2_27_CheckerboardPattern.c
@@ -0,0 +1,34 @@
+/*
+       Program: Ch2_Ex2_27_CheckerboardPattern.c
+   Descripción: This program displays a checkerboard pattern with eight printf
+                statements and then display the same pattern with as few printf
+		          statements as possible.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+	puts("Eight printf statements:");
+	printf("%s", "* * * * * * * *\n");
+	printf("%s", " * * * * * * * *\n");
+	printf("%s", "* * * * * * * *\n");
+	printf("%s", " * * * * * * * *\n");
+	printf("%s", "* * * * * * * *\n");
+	printf("%s", " * * * * * * * *\n");
+	printf("%s", "* * * * * * * *\n");
+	printf("%s", " * * * * * * * *\n\n");
+
+	puts("One printf statement:");
+	printf("%s", "* * * * * * * *\n * * * * * * * *\n"
+			       "* * * * * * * *\n * * * * * * * *\n"
+			       "* * * * * * * *\n * * * * * * * *\n"
+			       "* * * * * * * *\n * * * * * * * *\n");
+
+	return 0;
+
+}
