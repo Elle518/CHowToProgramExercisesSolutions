af2c33c9366a6f074446500ddc85177ef149b309
Elle518 <teregc@hotmail.es>
2018-05-24 17:07:13 +0200

    Correct indentation

--- Chapter3/Ch3_Ex3_6_SumIntegers.c
+++ Chapter3/Ch3_Ex3_6_SumIntegers.c
@@ -2,7 +2,7 @@
        Program: Ch3_Ex3_6_SumIntegers.c
    Descripción: This program calculates the sum of the integers from 1 to 10.
                 Use the while statement to loop through the calculation and
-		            increment statements. 
+                increment statements. 
          Autor: Elle518
 
                 v1.0
