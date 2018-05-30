d65b3f7805084fc443633ee36dfaedf1e9c06da3
Elle518 <teregc@hotmail.es>
2018-05-30 22:03:24 +0200

    Ch3_Ex3_25_TabularOutput.c

---
+++ Chapter3/Ch3_Ex3_25_TabularOutput.c
@@ -0,0 +1,25 @@
+/*
+       Program: Ch3_Ex3_25_TabularOutput.c
+   Descripción: This program utilizes looping to produce a table of numbers.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+   int number = 3;
+
+   printf("%s\n\n", "A\tA+2\tA+4\tA+6");
+
+   while (number <= 15) {
+      printf("%d\t%d\t%d\t%d\t\n", number, number +2, number + 4, number + 6);
+      number += 3;
+   }
+
+   return 0;
+
+}
