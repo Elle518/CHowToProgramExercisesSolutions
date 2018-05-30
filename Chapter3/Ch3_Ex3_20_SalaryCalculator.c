739969b9a3ad58a947e48e91ff51e8d54df84f0c
Elle518 <teregc@hotmail.es>
2018-05-30 16:45:57 +0200

    Ch3_Ex3_20_SalaryCalculator.c

---
+++ Chapter3/Ch3_Ex3_20_SalaryCalculator.c
@@ -0,0 +1,42 @@
+/*
+       Program: Ch3_Ex3_20_SalaryCalculator.c
+   Descripción: This program determines the gross pay for each of several
+                employees. The company pays “straight time” for the first 40
+                hours worked by each employee and pays “time-and-a-half” for
+                all hours worked in excess of 40 hours. 
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+   float hoursWorked;
+   float hourlyRate;
+   float salary;
+
+   printf("%s", "Enter # of hours worked (-1 to end): ");
+   scanf("%f", &hoursWorked);
+
+   while (hoursWorked != -1) {
+      printf("%s", "Enter hourly rate of the worker ($00.00): ");
+      scanf("%f", &hourlyRate);
+
+      if (hoursWorked <= 40) {
+         salary = hoursWorked * hourlyRate;
+      } else {
+         salary = 40 * hourlyRate + (hoursWorked - 40) * 1.5 * hourlyRate;
+      }
+
+      printf("Salary is $%.2f\n", salary);
+
+      printf("%s", "\nEnter # of hours worked (-1 to end): ");
+      scanf("%f", &hoursWorked);
+   }
+
+   return 0;
+
+}
