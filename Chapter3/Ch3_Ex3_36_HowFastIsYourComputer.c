dfef7938eb8c4b50e7201db544c7c93d6c87393b
Elle518 <teregc@hotmail.es>
2018-06-04 16:58:41 +0200

    Add exercise Ch3_Ex3_36_HowFastIsYourComputer.c

---
+++ Chapter3/Ch3_Ex3_36_HowFastIsYourComputer.c
@@ -0,0 +1,11 @@
+/*
+       Program: Ch3_Ex3_36_HowFastIsYourComputer.c
+   Descripción: This program uses a while loop that counts from 1 to
+                1,000,000,000 by 1s. Every time the count reaches a multiple of
+                100,000,000, prints that number on the screen. Use your watch
+                to time how long each 100 million iterations of the loop takes.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
