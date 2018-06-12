f8aad50d2dfa91339422a29466f158018ad058ad
Elle518 <teregc@hotmail.es>
2018-06-12 11:41:09 +0200

    Correct header comments

--- Chapter3/Ch3_Ex3_22_PrintingNumbersFromLoop.c
+++ Chapter3/Ch3_Ex3_22_PrintingNumbersFromLoop.c
@@ -1,7 +1,7 @@
 /*
        Program: Ch3_Ex3_22_PrintingNumbersFromLoop.c
    Descripción: This program utilizes looping to print the numbers from 1 to 10
-                side by side on the same line with three spaces between numbers.
+                side by side, on the same line, with three spaces between numbers.
          Autor: Elle518
 
                 v1.0
