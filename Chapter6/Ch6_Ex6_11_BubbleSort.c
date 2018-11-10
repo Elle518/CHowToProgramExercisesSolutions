1632fc741fb35a1c43ff7838d8b49927db99465b
Elle518 <teregc@hotmail.es>
2018-11-10 18:53:47 +0100

    Add exercise Ch6_Ex6_11_BubbleSort.c

---
+++ Chapter6/Ch6_Ex6_11_BubbleSort.c
@@ -0,0 +1,74 @@
+/*
+       Program: Ch6_Ex6_11_BubbleSort.c
+   Descripción: This program sorts an array’s values into ascending order using
+                the technique called the bubble sort or the sinking sort.
+         Autor: Elle518
+
+                v1.0
+                This code is in the public domain.
+*/
+
+#include <stdio.h>
+#define SIZE 10
+
+int main(void) {
+
+   int a[SIZE] = {2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+   //int a[SIZE] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+   int count = 1;
+   int swaps = 0;
+
+   puts("Data items in original order:");
+
+   for (size_t i = 0; i < SIZE; i++) {
+      printf("%4d", a[i]);
+   }
+
+   puts("");
+
+   // Bubble sort
+   for (unsigned int pass = 1; pass < SIZE; pass++) {
+
+      swaps = 0;
+
+      // Loop to control number of comparisons per pass
+      for (size_t i = 0; i < SIZE - count; i++) {
+
+         // Compare adjacent elements and swap them if first
+         // element is greater than second element
+         if (a[i] > a[i + 1]) {
+            int hold = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = hold;
+            swaps++;
+         }
+
+      }
+
+      if (swaps == 0) {
+        break;
+      }
+
+      // Display array after each pass
+      printf( "\nAfter Pass %d: ", pass ); 
+      
+      for ( size_t i = 0; i < SIZE - count; i++ ) {
+         printf( " %d", a[ i] );
+      }
+
+      // Every pass one less comparison is needed
+      count++;
+
+   }
+
+   puts("\n\nData items in ascending order:");
+
+   for (size_t i = 0; i < SIZE; ++i) {
+      printf("%4d", a[i]);
+   }
+
+   puts("");
+
+   return 0;
+
+}
